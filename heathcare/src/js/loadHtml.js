3b160e3a28fd934903737eb0a060077bb3210664
b-hyoung <youqlrqod@gmail.com>
2025-03-18 17:22:15 +0900

    add setting

---
+++ heathcare/src/js/loadHtml.js
@@ -0,0 +1,14 @@
+function loadHTML(selector, file) {
+    fetch(file)
+        .then(response => response.text())
+        .then(data => {
+            document.querySelector(selector).innerHTML = data;
+        })
+        .catch(error => console.error(`Error loading ${file}:`, error));
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadHTML('#idx_hero', '../pages/hero_banner.html');
+   
+});
